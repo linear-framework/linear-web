de118e11d6c685e17df6c69cc6c97640f167b505
Keith Policano <keith.policano@us.pwc.com>
2020-03-12 12:49:12 -0500

    add Access-Control-Allow-Credentials when CORS is enabled

--- src/main/scala/com/linearframework/web/internal/CorsFilter.scala
+++ src/main/scala/com/linearframework/web/internal/CorsFilter.scala
@@ -5,6 +5,7 @@ import com.linearframework.web._
 private[web] class CorsFilter(allowOrigin: String) extends Filter {
   BEFORE { (_, response) =>
     response.setHeader("Access-Control-Allow-Origin", allowOrigin)
+    response.setHeader("Access-Control-Allow-Credentials", "true")
   }
 }
 
