1609bb2e6c59f934e52d8dc92c2d5a7e6c4f6fb4
Keith Policano <keith.policano@us.pwc.com>
2020-05-21 20:58:06 -0500

    add SameSite.apply

--- src/main/scala/com/linearframework/web/SameSite.scala
+++ src/main/scala/com/linearframework/web/SameSite.scala
@@ -8,4 +8,13 @@ object SameSite {
   case object LAX extends SameSite("Lax")
   case object STRICT extends SameSite("Strict")
   case object NONE extends SameSite("None")
+
+  def apply(str: String): SameSite = {
+    str.trim.toUpperCase match {
+      case "LAX" => LAX
+      case "STRICT" => STRICT
+      case "NONE" => NONE
+      case _ => throw new IllegalArgumentException(s"Invalid SameSite option: $str")
+    }
+  }
 }
